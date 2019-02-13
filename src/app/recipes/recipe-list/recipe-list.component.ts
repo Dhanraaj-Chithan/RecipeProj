b7e5b9341b612c944dbc04f4708514a9f797567c
Dhanraaj-Chithan <dhanraajchithan@gmail.com>
2019-02-13 20:18:26 +0530

    V-II All Files

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,16 +8,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes :Recipe[] =[
     new Recipe('Cake','Dessert',
-    'https://commons.wikimedia.org/wiki/File:Recipe-575434.svg'),
+    'https://sallysbakingaddiction.com/wp-content/uploads/2013/04/triple-chocolate-cake-4.jpg'),
     new Recipe('Pasta','Italian',
-    'https://commons.wikimedia.org/wiki/File:Recipe-575434.svg')
+    'https://www.cscassets.com/recipes/wide_cknew/wide_26977.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
